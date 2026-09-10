ae344a9fa664a6757df3772621051b414d72f7ed
Patrick Häcker <patrick.haecker@bosch.com>
2026-09-10 13:05:14 +0200

    Work around for large multiplications involving `BitInteger`s

--- ext/BitIntegersExt.jl
+++ ext/BitIntegersExt.jl
@@ -1,7 +1,18 @@
 module BitIntegersExt
 
 using EmulatedBitIntegers: EmulatedInteger
+import BitIntegers
 using BitIntegers: AbstractBitSigned, AbstractBitUnsigned
+
+if VERSION < v"1.11-" && isdefined(BitIntegers, :broken_mul_with_overflow) &&
+   !hasmethod(BitIntegers.broken_mul_with_overflow, Tuple{T, T} where T<:AbstractBitUnsigned)
+	# TODO: Upstream the missing unsigned checked-multiplication fallback to BitIntegers.
+	function BitIntegers.broken_mul_with_overflow(x::T, y::T) where T<:AbstractBitUnsigned
+		product = BigInt(x) * BigInt(y)
+		result = product % T
+		return result, product > typemax(T)
+	end
+end
 
 (::Type{Target})(value::EmulatedInteger) where {Target<:Union{AbstractBitSigned, AbstractBitUnsigned}} = Target(value[])
 
--- test/bitintegers.jl
+++ test/bitintegers.jl
@@ -1,4 +1,32 @@
 @define_integers 24 ConversionInt24 ConversionUInt24
+@define_integers 128 MultiplicationInt128 MultiplicationUInt128
+
+@testset "BitIntegers checked multiplication" begin
+    @emulate UInt127 UInt128 UInt129 UInt255 UInt257 UInt513 UInt3_256
+    for Source in (MultiplicationUInt128, UInt256, UInt512, UInt1024,
+                   UInt127, UInt128, UInt129, UInt255, UInt257, UInt513, UInt3_256)
+        high = BigInt(typemax(Source))
+        root = isqrt(high)
+        values = unique([big(0), big(1), big(2), root, root + 1, high - 1, high])
+        for left in values, right in values
+            x, y = Source(left), Source(right)
+            product = left * right
+            overflow = product > high
+            @test (@inferred Base.Checked.mul_with_overflow(x, y)) === (product % Source, overflow)
+            if overflow
+                @test_throws OverflowError Base.Checked.checked_mul(x, y)
+            else
+                @test (@inferred Base.Checked.checked_mul(x, y)) === Source(product)
+            end
+            multiple = lcm(left, right)
+            if multiple > high
+                @test_throws OverflowError lcm(x, y)
+            else
+                @test lcm(x, y) === Source(multiple)
+            end
+        end
+    end
+end
 
 @testset "BitIntegers conversion" begin
     @emulate Int3 UInt3 Int3_128 UInt3_128 Int129 UInt129
